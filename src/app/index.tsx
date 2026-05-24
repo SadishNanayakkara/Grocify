40cb0d6ea3baf8704b15dad5439eb8ab1c9570fc
SadishNanayakkara <sswijerathnananayakkara@gmail.com>
2026-05-24 20:35:24 +0530

    chore: update dependencies and add Tailwind CSS support
    - Added babel-preset-expo and nativewind dependencies to package.json.
    - Removed unused About screen and layout files from the about directory.
    - Updated index.tsx to display an image and modified the text.
    - Created a tasks.json file for VSCode to streamline Expo start command.
    - Added global.css for Tailwind CSS styles.
    - Created tailwind.config.js for Tailwind CSS configuration.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,15 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>
-        Edit src/app/index.tsx to edit this screen. Hellow moda thiruni
-      </Text>
-
-      <Link href={"/about"}>About screen link</Link>
+      <Text>Grocify </Text>
+      <Image
+        source={require("../../assets/images/icon.png")}
+        style={{ width: 200, height: 200, borderRadius: 20 }}
+      />
     </View>
   );
 }
